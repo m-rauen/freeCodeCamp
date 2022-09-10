f2c0ef4f15386f36ced05ee13347aa5c08e1cac7
Matheus Rauen C. L <matheus.rauen@grad.ufsc.br>
2022-09-09 23:20:15 -0300

    Add README

---
+++ README.md
@@ -0,0 +1,9 @@
+# :books: freeCodeCamp
+
+### Basic Information 
+
+---
+
+Here are all the files with the solved problems, and challenges, proposed throughout the courses that I take at [freeCodeCamp](https://www.freecodecamp.org/learn/). The individuous courses are separated into folders, and, inside every single folder are the solved problems and challenges (in a separated dir).
+
+
--- Scientific_Computing_Python/M09-Exs.py
+++ Scientific_Computing_Python/M09-Exs.py
@@ -1,3 +1,61 @@
 #Dictionaries Chapter
 
 #Ex2
+days_data = {}
+
+fname = input('Enter a file name: ')
+
+for line in open(fname):
+    if line.startswith('From '):
+        new_line = line.split()
+        days_data.update({new_line[2]:new_line[4]})
+
+print(days_data)
+
+#Ex3 
+emails = {}
+
+fname = input('Enter file name: ')
+
+for line in open(fname):
+    if line.startswith('From '):
+        new_line = line.split()
+        if new_line[1] in emails:
+            emails[new_line[1]] += 1
+        else:
+            emails[new_line[1]] = 1
+            
+print('\n{}'.format(emails))
+
+#Ex4 
+emails = {}
+
+fname = input('Enter file name: ')
+
+for line in open(fname):
+    if line.startswith('From '):
+        new_line = line.split()
+        if new_line[1] in emails:
+            emails[new_line[1]] += 1
+        else:
+            emails[new_line[1]] = 1
+            
+print(emails)
+
+#Ex5 
+domain_emails = {}
+
+fname = input('Enter file name: ')
+
+for line in open(fname):
+    if line.startswith('From '):
+        new_line = line.split()
+        domain = new_line[1].split('@')
+        if domain[1] in domain_emails:
+            domain_emails[domain[1]] += 1
+        else:
+            domain_emails[domain[1]] = 1
+            
+print(domain_emails)
+        
+            
